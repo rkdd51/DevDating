949f42dd08f662c287b0b36d073113a98ae612de
Rahul Dubey <rahuldubay51@gmail.com>
2024-09-28 21:38:02 +0530

    Learning Request handlers

--- src/app.js
+++ src/app.js
@@ -2,37 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.use(
+  "/user",
+  (req, res, next) => {
+    // res.send("1st Response");
+    next();
+  },
+  (req, res, next) => {
+    res.send("2nd Response");
+    next();
+  },
+  (req, res, next) => {
+    res.send("3rd Response");
+    next();
+  }
+);
 
-app.get("/user", (req, res) => {
-    res.send({
-      name: "Rahul",
-      method: "GET",
-    });
-});
-app.post("/user", (req, res) => {
-  res.send({
-    name: "Rahul",
-    method: "POST",
-  });
-});
-
-app.put("/user", (req, res) => {
-   res.send({
-     name: "Rahul",
-     method: "PUT",
-   });
-});
-
-app.delete("/user", (req, res) => {
-   res.send({
-     name: "Rahul",
-     method: "DELETE",
-   });
-});
-
-app.use("/user", (req, res) => {
-  res.send("Hello User!");
-});
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
