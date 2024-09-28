4accb38c671ed611cb7b4a4013af3e5cfee8132d
Rahul Dubey <rahuldubay51@gmail.com>
2024-09-28 11:57:31 +0530

    Exploring GET,POST,PUT,DELETE

--- src/app.js
+++ src/app.js
@@ -2,16 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello World");
+
+app.get("/user", (req, res) => {
+    res.send({
+      name: "Rahul",
+      method: "GET",
+    });
+});
+app.post("/user", (req, res) => {
+  res.send({
+    name: "Rahul",
+    method: "POST",
+  });
 });
 
-app.use("/test", (req, res) => {
-  res.send("Test");
+app.put("/user", (req, res) => {
+   res.send({
+     name: "Rahul",
+     method: "PUT",
+   });
 });
 
-app.use("/", (req, res) => {
-  res.send("Hello Rahul");
+app.delete("/user", (req, res) => {
+   res.send({
+     name: "Rahul",
+     method: "DELETE",
+   });
+});
+
+app.use("/user", (req, res) => {
+  res.send("Hello User!");
 });
 
 app.listen(4000, () => {
