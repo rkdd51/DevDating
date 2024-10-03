5d3871172bc3be76b2a41da7d95c25b7e6be9ace
Rahul Dubey <rahuldubay51@gmail.com>
2024-10-03 10:44:07 +0530

    Getting single user info from email

--- src/app.js
+++ src/app.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const User = require("./model/user")
+const User = require("./model/user");
 
-app.use(express.json()); //Using this middleware to convert json object to javascript object 
+app.use(express.json()); //Using this middleware to convert json object to javascript object
 
-app.post('/signup', async (req, res) => {
-  const user = new User(req.body);
+app.get("/user", async (req, res) => {
+  let userEmail = req.body.emailId;
   try {
-    await user.save()
-    res.send("Data added successfully")
+    let user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.send("No Email Found");
+    } else {
+      res.send(user);
+    }
   } catch (err) {
-    console.log("Error saving user");
+    console.log(err);
+    res.status(500).send("Server Error");
   }
-})
+});
 
-   connectDB()
-     .then(() => {      
-       console.log("Database connection established");
-        app.listen(3000, () => {
-          console.log("Server is running on port 4000");
-        });
-     })
-     .catch((error) => {
-       console.log("Database connection failed: +++++", error);
-     });
+// app.post('/signup', async (req, res) => {
+//   const user = new User(req.body);
+//   try {
+//     await user.save()
+//     res.send("Data added successfully")
+//   } catch (err) {
+//     console.log("Error saving user");
+//   }
+// })
 
+connectDB()
+  .then(() => {
+    console.log("Database connection established");
+    app.listen(3000, () => {
+      console.log("Server is running on port 4000");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection failed: +++++", error);
+  });
