4c0d2659157f4be1d2d492efb1449e529cae641b
Rahul Dubey <rahuldubay51@gmail.com>
2024-10-20 15:50:59 +0530

    adding schema handlers

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./model/user");
 const { validateSignUpData } = require("./utils/validator");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
 const { userAuth } = require("../src/middlewares/auth"); 
-
+const bcrypt = require("bcrypt");
 
 app.use(express.json()); //Using this middleware to convert json object to javascript object
 app.use(cookieParser());
@@ -46,12 +44,12 @@ app.post("/login", async function (req, res) {
     if (!user) {
       return res?.status(404).send("User not found");
     }
-    const isPasswordMatch = await bcrypt?.compare(password, user?.password);
+    const isPasswordMatch = await user.getPasswordVerified(password);
     if (!isPasswordMatch) {
       return res.status(400).send("Invalid Credentials");
     }
     if (isPasswordMatch) {
-      const token = await jwt.sign({_id:user?._id},"PrivateKey");
+      const token = await user.getJWT();
       res.cookie("token", token);
       res.send("Login Successful");
     }
@@ -71,80 +69,6 @@ app.get("/profile", userAuth, async (req, res) => {
     res.status(200).send("Error fetching profile");
   }
 });
-
-
-// // //Find by Id and update
-// app.patch("/user/:userId", async (req, res) => {
-//   const user = req?.params?.userId;
-//   const updatedBody = req?.body;
-
-//   try {
-//     const allowedUpdates = ["firstName", "lastName", "age", "gender", "skills"];
-
-//     const isUpdateAllowed = Object.keys(updatedBody).every((k) =>
-//       allowedUpdates.includes(k)
-//     );
-    
-//     if (!isUpdateAllowed) {
-//       throw new Error("User update is not allowed");
-//     }
-//     if (updatedBody?.skills?.length > 10) {
-//       throw new Error("More than 10 skills are not allowed");
-//     }
-
-//     let updatedUser = await User.findByIdAndUpdate(user, updatedBody, {
-//       returnDocument: "before",
-//       runValidators: true,
-//     });
-//     res.send("Data updated successfully");
-//   } catch (err) {
-//     console.log("Error is" + err);
-//   }
-// });
-
-// //Find by id and delete
-// app.delete("/user", async (req, res) => {
-//   try {
-//     let userId = req.body.userId;
-//     if (!userId) {
-//       return res.status(400).send("No user id provided");
-//     } else {
-//       let user = await User.findByIdAndDelete(userId);
-//       res.send("User deleted successfully");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// // User that gets all user data from database
-// app.get("/feed", async (req, res) => {
-//   try {
-//     let users = await User.find();
-//     res.send(users);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// // User that gets one user data from database
-// app.get("/user", async (req, res) => {
-//   let userEmail = req.body.emailId;
-//   try {
-//     let user = await User.findOne({ emailId: userEmail });
-//     if (!user) {
-//       res.send("No Email Found");
-//     } else {
-//       res.send(user);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send("Server Error");
-//   }
-// });
-
-
-
 
 
 connectDB()
--- src/model/user.js
+++ src/model/user.js
@@ -1,55 +1,79 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 var validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    minLength: 3,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    minLength: 3,
-  },
-  emailId: {
-    type: String,
-    required: true, 
-    unique: true, 
-    trim:true,
-    validate(value) {
-      if (!validator.isEmail(value)){
-        throw new Error("Invalid Email")
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 18,
-    max:65,
-  },
-  gender: {
-    type: String,
-    validate: {
-      validator: (value) => {
-        return ['male', 'female', 'other'].includes(value.toLowerCase());
-      },
-      message: '{VALUE} is not a valid gender and this is not a generated message.',
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      minLength: 3,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email");
         }
-    // required: true,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 65,
+    },
+    gender: {
+      type: String,
+      validate: {
+        validator: (value) => {
+          return ["male", "female", "other"].includes(value.toLowerCase());
+        },
+        message:
+          "{VALUE} is not a valid gender and this is not a generated message.",
+      },
+      // required: true,
+    },
+    skills: {
+      type: [String],
+      default: ["javascript"],
+      required: false,
+    },
   },
-  skills: {
-    type: [String],
-    default: ["javascript"],
-    required: false,
-  },
-}, {
-  timestamps:true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-const userModel = mongoose.model('User', userSchema)
+userSchema.methods.getJWT = async function () {
+  const token = await jwt.sign({ _id: this._id }, "PrivateKey", {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+userSchema.methods.getPasswordVerified = async function (passwordInputByUser) {
+  const isPasswordMatch = await bcrypt?.compare(
+    passwordInputByUser,
+    this.password
+  );
+  if (!isPasswordMatch) {
+    return res.status(400).send("Invalid Credentials");
+  }
+  return isPasswordMatch;
+};
+
+const userModel = mongoose.model("User", userSchema);
 
 module.exports = userModel;
