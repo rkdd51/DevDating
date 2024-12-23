721654edfe83aead24544ff4fa49ef3e736a6c9e
Rahul Dubey <rahuldubay51@gmail.com>
2024-12-23 10:27:42 +0530

    "Request accept API "

--- src/routes/user.js
+++ src/routes/user.js
@@ -25,4 +25,32 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id },
+        { toUserId: loggedInUser._id },
+      ],
+      status: "accepted",
+    }).populate(
+      "fromUserId",
+      ["firstName", "lastName"]
+    );
+
+    let connectionDetails = connections.map((data)=>data?.fromUserId)
+
+    res.json({
+      message: "User connections",
+      connectionDetails,
+    });
+   
+  } catch (err) {
+    res.status(400).send("Error fetching user connections:" + err.message);
+  }
+});
+
 module.exports = userRouter;
